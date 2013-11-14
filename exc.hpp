c20e52596cff533723e009f2836b697047253cc6
Konstantin Sorokin <kvs@sigterm.ru>
2013-11-14 15:15:16 +0400

    more correct exception class.

--- exc.hpp
+++ exc.hpp
@@ -11,40 +11,37 @@
 
 namespace imgdupl {
 
+#if defined  __GNUC__ || defined __clang__
+#   define CHECK_PRINTF_ARGS(f, e)  __attribute__ ((format (printf, f, e)))
+#else
+#   define CHECK_PRINTF_ARGS(f, e)
+#endif
+
 class Exc: public std::exception {
 public:
 
-    Exc(const char *file, int line, const char *fmt, ...): msg(NULL)
+    Exc(const char *file, int line, const char *fmt, ...) CHECK_PRINTF_ARGS(4, 5)
     {
         va_list ap;
-        char    buff[1024];
-        size_t  len;
+        char    buff[512];
 
         va_start(ap, fmt);
         vsnprintf(buff, sizeof(buff), fmt, ap);
         va_end(ap);
 
-        len = 0;
-        memset(line_str, 0, sizeof(line_str));
-        snprintf(line_str, sizeof(line_str), "%i", line);
-        len = strlen(buff) + 4 /* ' at ' */ + strlen(file) + 1 /* ':' */ +
-            strlen(line_str) + 1 /* terminating zero */;
-        msg = static_cast<char*>(calloc(1, len));
-        snprintf(msg, len, "%s at %s:%s", buff, sanitize_file_name(file), line_str);
+        snprintf(msg, sizeof(msg), "%s:%i %s", sanitize_file_name(file), line, buff);
     }
 
     virtual ~Exc() throw() {
-        free(msg);
-    };
+    }
 
     virtual const char *what() const throw() {
-        return msg == NULL ? "" : msg;
+        return msg;
     }
 
 private:
 
-    char *msg;
-    char  line_str[sizeof("2147483647")];
+    char msg[512];
 
     Exc() {}
 
